3e1b3e945652ccce88e806606a38be7a5277153f
itzVort3x1 <saikaustubh10@gmail.com>
2023-06-17 18:00:56 +0530

    added DynamoDBv3

---
+++ DynamoDBv3/CreateTable.js
@@ -0,0 +1,27 @@
+const {
+	DynamoDBClient,
+	CreateTableCommand,
+} = require("@aws-sdk/client-dynamodb");
+
+const dbclient = new DynamoDBClient();
+
+const params = {
+	TableName: "Employee",
+	KeySchema: [
+		{ AttributeName: "Id", KeyType: "HASH" }, //Partition key
+	],
+	AttributeDefinitions: [{ AttributeName: "Id", AttributeType: "N" }],
+	ProvisionedThroughput: {
+		ReadCapacityUnits: 3,
+		WriteCapacityUnits: 3,
+	},
+};
+
+const run = async () => {
+	try {
+		const data = await dbclient.send(new CreateTableCommand(params));
+		console.log("Table is created", data);
+	} catch (err) {
+		console.log("Error", err);
+	}
+};
